31313dee7af7701981a945d4093406aec5f28510
= <zorguiabdarrahmen@gmail.com>
2025-05-20 18:56:13 +0100

    add form widget

--- lib/view/forget_pass_page/forget_pass_page.dart
+++ lib/view/forget_pass_page/forget_pass_page.dart
@@ -3,11 +3,13 @@ import 'package:get/get.dart';
 import 'package:my_patients/controller/authController.dart';
 import 'package:my_patients/core/constants/colors.dart';
 import 'package:my_patients/core/constants/text_styles.dart';
+import 'package:my_patients/core/input_validator.dart';
 import 'package:my_patients/view/widgets/my_button.dart';
 import 'package:my_patients/view/widgets/my_textfield.dart';
 
 class ForgetPassPage extends StatelessWidget {
-  const ForgetPassPage({super.key});
+  ForgetPassPage({super.key});
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
@@ -23,21 +25,38 @@ class ForgetPassPage extends StatelessWidget {
           padding: const EdgeInsets.all(20),
           child: ConstrainedBox(
             constraints: BoxConstraints(maxHeight: 700),
-            child: Column(
-              children: [
-                Text("Entrez votre email pour envoyer un lien de réinitialisation de mot de passe",style: AppTextStyles.detailText,textAlign: TextAlign.center,),
-                SizedBox(height: heigh*0.02,),
-                MyTextField(
-                  labelText: "Email",
-                  hintText: "Email",
-                  textController: controller.emailController,
-                ),
-                SizedBox(height: heigh*0.02,),
-                Obx(
-                  ()=> controller.isLoading.value? CircularProgressIndicator():
-                  MyButton(text: "Envoyer", onPressed: ()=>controller.recoverPassword(context))
-                ),
-              ],
+            child: Form(
+              key: _formKey,
+              child: Column(
+                children: [
+                  Text(
+                    "Entrez votre email pour envoyer un lien de réinitialisation de mot de passe",
+                    style: AppTextStyles.detailText,
+                    textAlign: TextAlign.center,
+                  ),
+                  SizedBox(height: heigh * 0.02),
+                  MyTextField(
+                    labelText: "Email",
+                    hintText: "Email",
+                    textController: controller.emailController,
+                    validator: InputValidator.validateEmail,
+                  ),
+                  SizedBox(height: heigh * 0.02),
+                  Obx(
+                    () =>
+                        controller.isLoading.value
+                            ? CircularProgressIndicator()
+                            : MyButton(
+                              text: "Envoyer",
+                              onPressed: () {
+                                if (_formKey.currentState!.validate()) {
+                                  controller.recoverPassword(context);
+                                }
+                              },
+                            ),
+                  ),
+                ],
+              ),
             ),
           ),
         ),
